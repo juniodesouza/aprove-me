7251dc81bcbbbbe06b1eaf9a4df51ba7cf318601
Junio de Souza <junioapdesouza@hotmail.com>
2025-06-15 12:45:21 -0300

    feat: add contact repository

---
+++ backend/src/assignor/domain/assignor.repository.ts
@@ -0,0 +1,8 @@
+import { AssignorEntity } from './assignor.entity'
+
+export abstract class AssignorRepository {
+  abstract create(assignor: AssignorEntity): Promise<AssignorEntity>
+  abstract findById(id: string): Promise<AssignorEntity | null>
+  abstract update(assignor: AssignorEntity): Promise<AssignorEntity>
+  abstract delete(id: string): Promise<void>
+}
---
+++ backend/src/payable/domain/payable.repository.ts
@@ -0,0 +1,8 @@
+import { PayableEntity } from './payable.entity'
+
+export abstract class PayableRepository {
+  abstract create(payable: PayableEntity): Promise<PayableEntity>
+  abstract findById(id: string): Promise<PayableEntity | null>
+  abstract update(payable: PayableEntity): Promise<PayableEntity>
+  abstract delete(id: string): Promise<void>
+}
