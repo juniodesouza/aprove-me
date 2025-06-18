b69f39010aeb8f8ace9949c47ab68590151cdadf
Junio de Souza <junioapdesouza@hotmail.com>
2025-06-18 19:49:14 -0300

    refactor: reorganize imports and enhance PayableList component structure

--- frontend/src/components/pages/payable/payable.list.tsx
+++ frontend/src/components/pages/payable/payable.list.tsx
@@ -1,7 +1,15 @@
+import { Link } from 'react-router-dom'
+import { Pencil, PlusIcon, Trash2 } from 'lucide-react'
+import {
+   Table,
+   TableBody,
+   TableCell,
+   TableHead,
+   TableHeader,
+   TableRow,
+} from '@/components/ui/table'
 import { Page } from '@/components/layout/page'
 import { Button } from '@/components/ui/button'
-import { PlusIcon } from 'lucide-react'
-import { Link } from 'react-router-dom'
 
 export function PayableList() {
    const createNewRegister = (
@@ -15,7 +23,45 @@ export function PayableList() {
 
    return (
       <Page title="Pagáveis" actions={createNewRegister}>
-         PayableList
+         <div className="my-3 rounded-md border">
+            <Table>
+               <TableHeader>
+                  <TableRow className="bg-muted/40">
+                     <TableHead>Id</TableHead>
+                     <TableHead>Data de emissão</TableHead>
+                     <TableHead>Valor</TableHead>
+                     <TableHead>Cedente</TableHead>
+                     <TableHead className="w-[8%] text-center">Ações</TableHead>
+                  </TableRow>
+               </TableHeader>
+               <TableBody>
+                  <TableRow className="even:bg-muted/40">
+                     <TableCell>1</TableCell>
+                     <TableCell>01/01/2023</TableCell>
+                     <TableCell>R$ 100,00</TableCell>
+                     <TableCell>Empresa XYZ</TableCell>
+                     <TableCell className="flex justify-center">
+                        <Button
+                           variant="ghost"
+                           size="icon"
+                           className="h-5 w-8"
+                           onClick={() => {}}
+                        >
+                           <Pencil />
+                        </Button>
+                        <Button
+                           variant="ghost"
+                           size="icon"
+                           className="h-5 w-8"
+                           onClick={() => {}}
+                        >
+                           <Trash2 className="text-destructive" />
+                        </Button>
+                     </TableCell>
+                  </TableRow>
+               </TableBody>
+            </Table>
+         </div>
       </Page>
    )
 }
