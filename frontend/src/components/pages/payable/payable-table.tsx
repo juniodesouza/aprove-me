184c555aa4b46185638511eaad3120756d22ea04
Junio de Souza <junioapdesouza@hotmail.com>
2025-06-18 20:16:50 -0300

    refactor(frontend): clean up formatting in PayableTable component

--- frontend/src/components/pages/payable/payable-table.tsx
+++ frontend/src/components/pages/payable/payable-table.tsx
@@ -1,83 +1,89 @@
 import { Pencil, Trash2 } from 'lucide-react'
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
+   Table,
+   TableBody,
+   TableCell,
+   TableHead,
+   TableHeader,
+   TableRow,
 } from '@/components/ui/table'
 import { Button } from '@/components/ui/button'
 import { format } from 'date-fns'
 
 interface Payable {
-  id: string
-  emissionDate: Date
-  value: number
-  assignorId: string
+   id: string
+   emissionDate: Date
+   value: number
+   assignorId: string
 }
 
 interface Assignor {
-  id: string
-  name: string
+   id: string
+   name: string
 }
 
 interface PayableTableProps {
-  payables?: Payable[]
-  assignors?: Assignor[]
+   payables?: Payable[]
+   assignors?: Assignor[]
 }
 
-export function PayableTable({ payables = [], assignors = [] }: PayableTableProps) {
-  return (
-    <div className="my-3 rounded-md border">
-      <Table>
-        <TableHeader>
-          <TableRow className="bg-muted/40">
-            <TableHead>Id</TableHead>
-            <TableHead>Data de emissão</TableHead>
-            <TableHead>Valor</TableHead>
-            <TableHead>Cedente</TableHead>
-            <TableHead className="w-[8%] text-center">Ações</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {payables.map((payable) => (
-            <TableRow key={payable.id} className="even:bg-muted/40">
-              <TableCell>{payable.id}</TableCell>
-              <TableCell>
-                {format(payable.emissionDate, 'dd/MM/yyyy')}
-              </TableCell>
-              <TableCell>
-                {new Intl.NumberFormat('pt-BR', {
-                  style: 'currency',
-                  currency: 'BRL',
-                }).format(payable.value / 100)}
-              </TableCell>
-              <TableCell>
-                {assignors.find((a) => a.id === payable.assignorId)?.name}
-              </TableCell>
-              <TableCell className="flex justify-center">
-                <Button
-                  variant="ghost"
-                  size="icon"
-                  className="h-5 w-8"
-                  onClick={() => {}}
-                >
-                  <Pencil />
-                </Button>
-                <Button
-                  variant="ghost"
-                  size="icon"
-                  className="h-5 w-8"
-                  onClick={() => {}}
-                >
-                  <Trash2 className="text-destructive" />
-                </Button>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </div>
-  )
+export function PayableTable({
+   payables = [],
+   assignors = [],
+}: PayableTableProps) {
+   return (
+      <div className="my-3 rounded-md border">
+         <Table>
+            <TableHeader>
+               <TableRow className="bg-muted/40">
+                  <TableHead className="w-[35%]">Id</TableHead>
+                  <TableHead>Data de emissão</TableHead>
+                  <TableHead>Valor</TableHead>
+                  <TableHead>Cedente</TableHead>
+                  <TableHead className="w-[8%] text-center">Ações</TableHead>
+               </TableRow>
+            </TableHeader>
+            <TableBody>
+               {payables.map((payable) => (
+                  <TableRow key={payable.id} className="even:bg-muted/40">
+                     <TableCell>{payable.id}</TableCell>
+                     <TableCell>
+                        {format(payable.emissionDate, 'dd/MM/yyyy')}
+                     </TableCell>
+                     <TableCell>
+                        {new Intl.NumberFormat('pt-BR', {
+                           style: 'currency',
+                           currency: 'BRL',
+                        }).format(payable.value / 100)}
+                     </TableCell>
+                     <TableCell>
+                        {
+                           assignors.find((a) => a.id === payable.assignorId)
+                              ?.name
+                        }
+                     </TableCell>
+                     <TableCell className="flex justify-center">
+                        <Button
+                           variant="ghost"
+                           size="icon"
+                           className="h-5 w-8"
+                           onClick={() => {}}
+                        >
+                           <Pencil />
+                        </Button>
+                        <Button
+                           variant="ghost"
+                           size="icon"
+                           className="h-5 w-8"
+                           onClick={() => {}}
+                        >
+                           <Trash2 className="text-destructive" />
+                        </Button>
+                     </TableCell>
+                  </TableRow>
+               ))}
+            </TableBody>
+         </Table>
+      </div>
+   )
 }
